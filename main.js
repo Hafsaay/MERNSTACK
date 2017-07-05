b451eeff4294fed5b49652ec9186eb17c6308491
Hafsaay <hafsa.shaikh76.hs@gmail.com>
2017-07-05 15:11:07 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-function printValue(){
-  var input = document.getElementById('input');
-  var input1 = document.getElementById('input1');
-  var input2 = document.getElementById('input2');
-  var container = document.getElementById('container');
+var e_no=new Array();
+var names=new Array();
+var surname=new Array();
+var myArray = [ e_no, names, surname ];
+function insert(){
+    var e_Value = document.getElementById('e_no').value;
+    var e_name_Value = document.getElementById('e_name').value;
+    var Sur_Value = document.getElementById('Surname').value;
+    e_no[e_no.length]=e_Value;
+    names[names.length]=e_name_Value;
+    surname[surname.length]=Sur_Value;
+  }
+  function show() {
+  var content="<b>All Elements of the Arrays :</b><br>";
+  for(var i = 0; i < e_no.length; i++) {
+     content +=e_no[i];
+  }
+  for(var i = 0; i < names.length; i++) {
+     content +=names[i];
+  }
+  for(var i = 0; i < surname.length; i++) {
+     content +=surname[i];
+  }
+  content= myArray;
+  document.getElementById('display').innerHTML = myArray;
 
-  container.innerHTML ="<h1>"+ input.value+"</br>"+input1.value+"</br>"+
-  input2.value+"</h1>" ;
 }
---
+++ main_one.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var str = "A quick brown fox jumps over a lazy dog.";
+    var res = str.split(" ");
+    document.getElementById("demo").innerHTML = res;
+}
